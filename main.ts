b4375d87f0d1beb804cc478fed6fa7e0235ad3d3
Azirinoxx <93145316+Azirinoxx@users.noreply.github.com>
2021-11-05 11:25:42 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
+let NB_de_personne = 0
 input.onButtonPressed(Button.A, function () {
     NB_de_personne = NB_de_personne + 1
     if (NB_de_personne == 10) {
-        basic.showString("Nombre de personne maximal attint")
+        basic.showString("X")
     }
     basic.showNumber(NB_de_personne)
+})
+input.onButtonPressed(Button.AB, function () {
+    NB_de_personne = 0
 })
 input.onButtonPressed(Button.B, function () {
     NB_de_personne = NB_de_personne - 1
     basic.showNumber(NB_de_personne)
 })
-let NB_de_personne = 0
-NB_de_personne = 0
